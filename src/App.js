6e3a4176ec67eed632e57b78fa7793f59b3d8f87
jangroy <jangroy@hotmail.com>
2017-10-08 23:37:31 -0700

    needs fixing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Grid, Row, Col } from 'react-bootstrap';
+// import logo from './logo.svg';
 import './App.css';
+
+// 1. what state is there?
+//    - pressing button updates screen to button
+// 2. when does it change?
+
+class Calculator extends React.Component {
+  state = {
+    displayValue: '0',
+    value1: 0,
+    value2: 0
+  }
+
+  // state changer
+  inputDigit(digit) {
+    const { displayValue } = this.state
+
+    this.setState({
+      displayValue: displayValue === '0' ? String(digit) : displayValue + digit
+    })
+  }
+  
+  add(digit) {
+    const { displayValue } = this.state
+    const { value1 } = this.state
+    const { value2 } = this.state
+    this.setState({
+      value1: value1 === 0 ? parseInt(digit) : value1 + parseInt(displayValue),
+      displayValue: '0'
+    })
+  }
+  
+  equal(digit) {
+    const { displayValue } = this.state
+    const { value1 } = this.state
+    const { value2 } = this.state
+    
+    this.setState({
+      value2: value1 + parseInt(displayValue),
+      displayValue: value2,
+    })
+
+  }
+
+  reset() {
+    const { displayValue } = this.state
+    const { value1 } = this.state
+    const { value2 } = this.state
+    
+    this.setState({
+      displayValue: '0',
+      value1: 0,
+      value2: 0
+    })
+  }
+
+  render(){
+    const { displayValue } = this.state
+    const { value1 } = this.state
+    const { value2 } = this.state
+
+    return (
+      <div>
+        <div className="divStyle">
+          {displayValue}
+        </div>
+
+        <button onClick={() => this.inputDigit(0)}>0</button>
+        <button onClick={() => this.inputDigit(1)}>1</button>
+        <button onClick={() => this.inputDigit(2)}>2</button>
+        <button onClick={() => this.inputDigit(3)}>3</button>
+        <button onClick={() => this.inputDigit(4)}>4</button>
+        <button onClick={() => this.inputDigit(5)}>5</button>
+        <button onClick={() => this.inputDigit(6)}>6</button>
+        <button onClick={() => this.inputDigit(7)}>7</button>
+        <button onClick={() => this.inputDigit(8)}>8</button>
+        <button onClick={() => this.inputDigit(9)}>9</button>
+        <button onClick={() => this.reset()}>reset</button>
+        <button onClick={() => this.add(displayValue)}>+</button>
+        <button onClick={() => this.equal(displayValue)}>=</button>
+        
+        <p>displayValue: {displayValue}</p>
+        <p>value1: {value1}</p>
+        <p>value2: {value2}</p>
+      
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Calculator />
       </div>
     );
   }
---
+++ src/savedStyle.js
@@ -0,0 +1,74 @@
+<Grid className="gridStyle">
+<Row className="screenRow">
+  <Col className="colStyle2" xs="12">
+    <Screen />
+  </Col>
+</Row>
+<Row>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">C</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">%</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">+/-</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle4">%</Button>
+  </Col>
+</Row>
+<Row>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">7</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">8</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">9</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle4">X</Button>
+  </Col>
+</Row>
+<Row>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">4</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">5</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">6</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle4">-</Button>
+  </Col>
+</Row>
+<Row>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">1</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">2</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">3</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle4">+</Button>
+  </Col>
+</Row>
+<Row>
+  <Col className="colStyle" xs="6">
+    <Button className="butStyle2">0</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle">.</Button>
+  </Col>
+  <Col className="colStyle" xs="3">
+    <Button className="butStyle4">=</Button>
+  </Col>
+</Row>
+</Grid>
